c2a1d3465655941a8313dbb5eafba3ae561ec3f9
parham <parham@datastack.academy>
2024-08-07 20:58:15 -0400

    adding more advanced decorators

--- lessons/20240807_decorators.py
+++ lessons/20240807_decorators.py
@@ -15,3 +15,25 @@ def print_msg(msg):
     print(msg)
 
 print_msg("hello decorators!")
+
+
+"""
+more advanced decorator example
+"""
+
+def repeat(n: int = 1):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for x in range(n):
+                print(f"call #{(x + 1):02}")
+                result = func(*args, **kwargs)
+            return result
+        # return
+        return wrapper
+    return decorator
+
+@repeat(5)
+def say_hi(name):
+    print(f"hi {name}")
+
+say_hi('Alice')
